988b185645de1d59de9271681b2eff4f69d9a230
java <java>
2016-07-21 15:51:21 +0900

    first commit

---
+++ src/main/java/com/hybrid/Chap03Application.java
@@ -0,0 +1,12 @@
+package com.hybrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap03Application.class, args);
+	}
+}
---
+++ src/test/java/com/hybrid/Chap03ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.hybrid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Chap03Application.class)
+@WebAppConfiguration
+public class Chap03ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
